e399f0a815a6452c143c16f1174b91dfbe244978
Shivraj Rajbhar <156070949+shivrajr1@users.noreply.github.com>
2026-03-04 23:53:33 +0530

    Fix syntax error in CORS configuration

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app =express()
 app.use(cors({
     origin:process.env.NODE_ENV === "production"
     ? process.env.FRONTEND_URL
-    : "http://localhost:5000";
+    : "http://localhost:5000"
 }))
 app.use(express.json())
 
